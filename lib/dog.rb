66e5b9259d0ae886899ef64e3b16bb6bb491e505
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-04 04:34:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,13 @@ class Dog
   end
   def name=(new_name)
     @name = new_name
+  def initialize(breed)
+    @breed = breed
+  end
   def breed 
-    
+    @breed
+  end
+  def breed=(new_breed)
+    @breed = new_breed
   end
 end
